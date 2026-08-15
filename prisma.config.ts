8adfe51bce1926312ee96329d1b652765e5f05f7
Deivid Santos Lahasse <deividlahasse@gmail.com>
2026-08-15 10:34:16 -0300

    debug: log temporario da database_url

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,11 @@
 import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
 
+console.log("DEBUG DATABASE_URL:", process.env.DATABASE_URL);
+
 export default defineConfig({
   engine: 'classic',
   datasource: {
-    url:env('DATABASE_URL'),
+    url: env('DATABASE_URL'),
   },
 })
