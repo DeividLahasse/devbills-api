86e8caa4782b0068c141c8b2f6f83b3abd518b18
Deivid Santos Lahasse <deividlahasse@gmail.com>
2026-08-15 10:39:32 -0300

    chore: remove log de debug

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,6 @@
 import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
 
-console.log("DEBUG DATABASE_URL:", process.env.DATABASE_URL);
 
 export default defineConfig({
   engine: 'classic',
